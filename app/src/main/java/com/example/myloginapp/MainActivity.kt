159c83e86a2b5e76ce5c6a03e1e913a66ccfda39
lmodenez <lmodenez98@gmail.com>
2021-11-10 19:48:55 -0300

    initial commit

---
+++ app/src/main/java/com/example/myloginapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myloginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val username = findViewById<TextView>(R.id.username);
+//        val password = findViewById<TextView>(R.id.password);
+//
+//        val loginBtn = findViewById<MaterialButton>(R.id.loginbtn)
+//
+//        loginBtn.setOnClickListener(View.OnClickListener {
+//            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+//                Toast.makeText(this,"Logado com sucesso", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this,"Falha ao realizar o login", Toast.LENGTH_SHORT).show()
+//            }
+//        })
+
+    }
+}
